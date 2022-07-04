487af2acf9ed0d9dd07d300a92faa03f0d7f2885
goutham <gouthamshine@gmail.com>
2022-07-04 16:33:34 +0530

    Initial Commit

---
+++ CustomViewBuilder/CustomViewBuilderApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomViewBuilderApp.swift
+//  CustomViewBuilder
+//
+//  Created by Goutham S on 04/07/22.
+//
+
+import SwiftUI
+
+@main
+struct CustomViewBuilderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
